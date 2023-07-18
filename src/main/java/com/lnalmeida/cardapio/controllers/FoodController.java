8a7b496e293173e68b3506fd4e9bcdf36581ee4e
Luiz Almeida <l.n.almeida.ti@gmail.com>
2023-07-18 14:44:30 -0300

    CREATED: CRUD methods, service, functional API

--- src/main/java/com/lnalmeida/cardapio/controllers/FoodController.java
+++ src/main/java/com/lnalmeida/cardapio/controllers/FoodController.java
@@ -1,13 +1,21 @@
 package com.lnalmeida.cardapio.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lnalmeida.cardapio.dto.FoodDTO;
+import com.lnalmeida.cardapio.entities.Food;
 import com.lnalmeida.cardapio.service.FoodService;
 
 
@@ -17,11 +25,46 @@ public class FoodController {
 	
 	@Autowired
 	FoodService foodService;
-	
+		
 	@GetMapping
 	public List<FoodDTO> getAllFoods() {
-		List<FoodDTO> foodList = foodService.getAllFoods();
+		List<FoodDTO> foodList = foodService.findAllFoods();
 		return foodList;
 	}
+	
+	@GetMapping(value= "/{id}")
+	public ResponseEntity<FoodDTO> getFoodById(@PathVariable Long id) {
+		Optional<FoodDTO> food = foodService.findFoodById(id);
+		if(food.isPresent()) {
+			return ResponseEntity.ok(food.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 
+	@PostMapping
+	public ResponseEntity<String> postNewFood(@RequestBody Food body) {
+		try {
+			foodService.createNewFood(body);
+			return ResponseEntity.ok("Registes created successfully");
+		} catch (Exception e) {
+			return (ResponseEntity<String>) ResponseEntity
+					.internalServerError()
+					.body("Error on create register.");
+		}		
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<FoodDTO> putFood(@PathVariable Long id, @RequestBody Food body) {
+		Optional<FoodDTO> food = foodService.editFood(id, body);
+		if(food.isPresent()) {
+			return ResponseEntity.ok(food.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public void deleteFood(@PathVariable Long id) {
+		foodService.deleteFood(id);
+	}
 }
--- src/main/java/com/lnalmeida/cardapio/service/FoodService.java
+++ src/main/java/com/lnalmeida/cardapio/service/FoodService.java
@@ -1,16 +1,18 @@
 package com.lnalmeida.cardapio.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lnalmeida.cardapio.dto.FoodDTO;
 import com.lnalmeida.cardapio.entities.Food;
 import com.lnalmeida.cardapio.repository.FoodRepository;
 
-import jakarta.transaction.Transactional;
+
 
 @Service
 public class FoodService {
@@ -18,10 +20,42 @@ public class FoodService {
 	@Autowired
 	FoodRepository foodRepository;
 	
-	@Transactional
-	public List<FoodDTO> getAllFoods() {
+	@Transactional(readOnly=true)
+	public List<FoodDTO> findAllFoods() {
 		List<Food> foodList = foodRepository.findAll();
 		return foodList.stream().map(food -> new FoodDTO(food)).collect(Collectors.toList());
 	}
-
+	
+	@Transactional(readOnly = true)
+	public Optional<FoodDTO> findFoodById(Long foodId) {
+		Optional<Food> food = foodRepository.findById(foodId);
+			return food.map(x -> new FoodDTO(x));		 
+	}
+	
+	@Transactional
+	public void createNewFood(Food food) {
+			foodRepository.save(food);
+	}
+	
+	@Transactional
+	public Optional<FoodDTO> editFood(Long id, Food foodData) {
+		Optional<Food> actualFood = foodRepository.findById(id);
+		if(actualFood.isPresent()) {
+			Food updatedFood = actualFood.get();
+			
+			updatedFood.setTitle(foodData.getTitle());
+			updatedFood.setImgUrl(foodData.getImgUrl());
+			updatedFood.setPrice(foodData.getPrice());
+			updatedFood.setDescription(foodData.getDescription());
+			
+			Food savedFood = foodRepository.save(updatedFood);
+			return Optional.of(new FoodDTO(savedFood));
+		}
+		return Optional.empty();		
+	}
+	
+	@Transactional
+	public void deleteFood(Long foodId) {
+			foodRepository.deleteById(foodId);	
+	}
 }
